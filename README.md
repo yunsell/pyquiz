e3a89feddcd092689554c6a2b459db42e845eb4a
yoonsi <yunsell31@gmail.com>
2020-12-19 00:38:29 +0900

    "pyquiz 1.1 ver"

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 - 파이썬 기초 문제 사이트입니다.
 ---
 
+# pyquiz 1.1 ver
+- admin 페이지 완료 (admin / 1234)
+- question_list 출력 완료
+```
 # pyquiz 1.0 ver
 - python, django 환경설정 완료
 - localhost:8000 구동완료
--- pyquiz/views.py
+++ pyquiz/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("안녕하세요 pyquiz에 오신걸 환영합니다.")
+    """
+    pyquiz 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date') # 질문목록 데이터//작성일시 역순
+    context = {'question_list': question_list}
+    return render(request, 'pyquiz/question_list.html', context)
+
+def detail(request, question_id):
+    """
+    pyquiz 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pyquiz/question_detail.html', context)
