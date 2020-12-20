3b5fbd34e77e1255d97296d38950c00dbd4aa3bf
yoonsi <yunsell31@gmail.com>
2020-12-20 22:48:13 +0900

    "pyquiz 1.2 ver"

--- pyquiz/views.py
+++ pyquiz/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Question
 
 
@@ -17,3 +18,11 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pyquiz/question_detail.html', context)
+
+def answer_create(request, question_id):
+    """
+    pyquiz 답변 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pyquiz:detail', question_id=question.id)
