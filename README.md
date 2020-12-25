a6e32e62de2ab9d53b730b5b734c7f04f8c12cf5
yoonsi <yunsell31@gmail.com>
2020-12-26 00:02:57 +0900

    pyquiz 1.3 ver

--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 # pyquiz
+파이썬 기초 문제풀이 & 커뮤니티 사이트입니다.
 
-- 파이썬 기초 문제 사이트입니다.
 ---
 
-# pyquiz 1.2 ver
+## pyquiz 1.3 ver
+- 게시글 작성 기능 완료
+- 게시글, 답변 없을시 오류 표시
+```
+## pyquiz 1.2 ver
 - base.html 적용
 - 게시판, 답변 기능 완료
 - bootstrap 적용
 ```
-# pyquiz 1.1 ver
+```
+## pyquiz 1.1 ver
 - admin 페이지 완료 (admin / 1234)
 - question_list 출력 완료
 ```
 ```
-# pyquiz 1.0 ver
+## pyquiz 1.0 ver
 - python, django 환경설정 완료
 - localhost:8000 구동완료
 ```
--- pyquiz/views.py
+++ pyquiz/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
+from .forms import QuestionForm, AnswerForm
 
 
 def index(request):
@@ -24,5 +25,31 @@ def answer_create(request, question_id):
     pyquiz 답변 등록
     """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pyquiz:detail', question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pyquiz:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pyquiz/question_detail.html', context)
+
+def question_create(request):
+    """
+    pyquiz 질문등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pyquiz:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pyquiz/question_form.html', context)
