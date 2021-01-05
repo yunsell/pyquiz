b66b906deeb12213c803df5748299c95f89c9c58
yoonsi <yunsell31@gmail.com>
2021-01-06 00:04:31 +0900

    pyquiz 2.1 ver

--- pyquiz/models.py
+++ pyquiz/models.py
@@ -2,21 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE) #계정이 삭제되면 계정과 연결된 Question 모델 데이터를 모두 삭제
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField(max_length=200) #제한된 텍스트는 CharField 를 사용
     content = models.TextField() #무제한 텍스트
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question') # voter 추가
 
     def __str__(self):
         return self.subject
 
 class Answer(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
     question = models.ForeignKey(Question, on_delete=models.CASCADE) #question과 연결된 answer도 삭제
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
 
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
