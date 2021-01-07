c74e40cdf9a73b2a36524da6c4380433ca368601
seil <yunsell31@gmail.com>
2021-01-08 00:53:07 +0900

    pyquiz 2.2 ver

--- pyquiz/views/answer_views.py
+++ pyquiz/views/answer_views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
 from django.utils import timezone
 
 from ..forms import AnswerForm
@@ -20,7 +20,7 @@ def answer_create(request, question_id):
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
-            return redirect('pyquiz:detail', question_id=question.id)
+            return redirect('{}#answer_{}'.format(resolve_url('pyquiz:detail', question_id=question.id), answer.id))
     else:
         form = AnswerForm()
     context = {'question': question, 'form': form}
@@ -43,7 +43,7 @@ def answer_modify(request, answer_id):
             answer.author = request.user
             answer.modify_date = timezone.now()
             answer.save()
-            return redirect('pyquiz:detail', question_id=answer.question.id)
+            return redirect('{}#answer_{}'.format(resolve_url('pyquiz:detail', question_id=answer.question.id), answer.id))
     else:
         form = AnswerForm(instance=answer)
     context = {'answer': answer, 'form': form}
