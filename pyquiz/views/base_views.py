2f461bfa3a1e47d262b8e7278cc2a310214e3251
yoonsi <yunsell31@gmail.com>
2021-01-12 23:15:56 +0900

    pyquiz 2.4 ver

--- pyquiz/views/base_views.py
+++ pyquiz/views/base_views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from ..models import Question, Answer, Comment
 from django.core.paginator import Paginator
+from django.db.models import Q, Count
 
 
 def index(request):
@@ -10,15 +11,31 @@ def index(request):
     """
     # 입력 파라미터
     page = request.GET.get('page', '1') # 페이지
+    kw = request.GET.get('kw', '') # 검색어
+    so = request.GET.get('so', 'recent') # 정렬기준
 
-    # 조회
-    question_list = Question.objects.order_by('-create_date') # 질문목록 데이터//작성일시 역순
+    # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else: # recent 인기순일시 댓글이 많이 달린 순서로 정렬 된다.
+        question_list = Question.objects.order_by('-create_date')
+
+    # 검색
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw) |  # 내용검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이검색
+        ).distinct()
 
     # 페이징처리
-    paginator = Paginator(question_list, 10) # 페이지당 1개씩 보여주기
+    paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw, 'so': so}
 
     return render(request, 'pyquiz/question_list.html', context)
 
